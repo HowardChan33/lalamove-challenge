036db001243793728933773dba7914e47dcd9a13
HowardChan33 <50632371+HowardChan33@users.noreply.github.com>
2021-05-31 18:42:16 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install nodejs npm
-sudo npm install
-sudo npm link
 sudo apt install sqlite3
+sudo npm install -g
+sudo npm link -g
 sqlite3 llmdatabase.db "CREATE TABLE llm_order (from_location TEXT, to_location TEXT, id INTEGER PRIMARY KEY, is_taken TEXT DEFAULT 0);"
